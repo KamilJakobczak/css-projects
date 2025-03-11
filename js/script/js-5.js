3a65f75e6846101eab9e6fc78bbc56b5497ca885
Kamil Jakóbczak <k_j1@vp.pl>
2025-03-11 13:02:36 +0100

    add expanding circles

---
+++ js/script/js-5.js
@@ -0,0 +1,17 @@
+const circle1JS5 = document.querySelector(
+	'.circle1-js5'
+);
+const circle2JS5 = document.querySelector(
+	'.circle2-js5'
+);
+
+window.addEventListener('scroll', () => {
+	const value = window.scrollY;
+	console.log(value);
+	circle1JS5.style.clipPath = `circle(${
+		150 * value * 0.0016
+	}px at 0 0)`;
+	circle2JS5.style.clipPath = `circle(${
+		150 * value * 0.0016
+	}px at 100% 100%)`;
+});
